da5843ae53fcbff68d75beef7020f5a77b8046b6
Diego R. Lima <contact@diegoribeiro.net>
2020-09-19 18:49:53 -0300

    Project started :D

---
+++ resources/lang/en/common.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'response' => [
+        'not_found' => 'The requested operation failed because a resource associated with the request could not be found.',
+        'too_many_requests' => 'Too many requests have been sent within a given time span.',
+        'unauthorized' => 'The user is not authorized to make the request.',
+        'forbidden' => 'The requested operation is forbidden and cannot be completed.',
+        'internal_server_error' => 'The request failed due to an internal error.',
+        'invalid' => 'Invalid data was entered.',
+        'conflict' => 'The request could not be completed due to a conflict with the current state of the target resource.',
+    ],
+];
