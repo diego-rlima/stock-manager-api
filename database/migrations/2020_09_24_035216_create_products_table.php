2f81a5989ee1f3c87b1275a1ac3b1234306c9942
Diego R. Lima <contact@diegoribeiro.net>
2020-09-24 02:38:57 -0300

    Creation of the products and stock migrations

---
+++ database/migrations/2020_09_24_035216_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku')->unique()->nullable();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('price', 9, 2)->nullable();
+            $table->decimal('promotional_price', 9, 2)->nullable();
+            $table->integer('in_stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
