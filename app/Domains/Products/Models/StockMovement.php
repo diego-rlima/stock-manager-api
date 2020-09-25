c3a4c2aa372a1ad3e003e6f644c42d0ef5a8993d
Diego R Lima <diegolima@plantaoativo.com>
2020-09-25 17:21:13 -0300

    Update to make the stock movements searchable

--- app/Domains/Products/Models/StockMovement.php
+++ app/Domains/Products/Models/StockMovement.php
@@ -2,11 +2,15 @@
 
 namespace App\Domains\Products\Models;
 
-use App\Support\Domains\Model;
+use Exception;
+use Carbon\Carbon;
+use App\Support\Domains\SearchableModel;
 use Database\Factories\StockMovementFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Support\Domains\Search\Contracts\SearchContract;
+use App\Support\Domains\Search\Contracts\AdvancedSearchContract;
 
-class StockMovement extends Model
+class StockMovement extends SearchableModel
 {
     use HasFactory;
 
@@ -16,6 +20,13 @@ class StockMovement extends Model
      * @var array
      */
     protected $fillable = ['qty'];
+
+    /**
+     * The attributes that are searchable.
+     *
+     * @var array
+     */
+    protected array $searchable = [];
 
     /**
      * The product that owns this movement.
@@ -35,5 +46,36 @@ class StockMovement extends Model
     protected static function newFactory()
     {
         return StockMovementFactory::new();
+    }
+
+    /**
+     * Add the search mutators.
+     *
+     * @param  \App\Support\Domains\Search\Contracts\SearchContract  $search
+     * @return void
+     */
+    protected function searchMutators(SearchContract $search): void
+    {
+        $search->addMutator('created_at', function ($value) {
+            try {
+                return !empty($value)
+                    ? Carbon::parse($value)->toDateString()
+                    : null;
+            } catch (Exception $exception) {
+                return null;
+            }
+        });
+    }
+
+    /**
+     * Configure the advanced search columns.
+     *
+     * @param  \App\Support\Domains\Search\Contracts\AdvancedSearchContract  $search
+     * @return void
+     */
+    protected function searchAdvancedSettings(AdvancedSearchContract $search): void
+    {
+        $search->addColumn('start', 'created_at', '>=')
+            ->addColumn('end', 'created_at', '<=');
     }
 }
